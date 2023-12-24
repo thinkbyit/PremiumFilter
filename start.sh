8a2c28183766c48eb1604727eb73304e830c64a9
jugnucode <101200766+jugnucode@users.noreply.github.com>
2023-12-24 08:52:28 +0530

    something

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/jugnucode/PremiumFilter.git /PremiumFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /PremiumFilter
 fi
-cd /PiroAutoFilterBot
+cd /PremiumFilter
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting PremiumFilter...."
 python3 bot.py
